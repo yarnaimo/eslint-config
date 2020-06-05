19e50ff246caca76cc869ad8d706f63aa56a703c
yamaimo <yarnaimo@gmail.com>
2020-06-06 02:20:50 +0900

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint", "react"],
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+
+    "no-undef": "off",
+  },
+};
+
+module.exports = config;
--- jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    ...require('@yarnaimo/tss/jest.config.js'),
-    setupFilesAfterEnv: ['<rootDir>/node_modules/@yarnaimo/tss/jest.setup.js'],
-}
