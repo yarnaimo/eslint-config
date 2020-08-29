df72c43786383420ae635083404b7f67471651d2
yamaimo <yarnaimo@gmail.com>
2020-08-29 11:22:29 +0900

    no-floating-promises / ban-ts-comment desc

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const config = {
     },
     rules: {
         'react/prop-types': 'off',
+        'react/display-name': 'off',
 
         '@typescript-eslint/camelcase': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
@@ -33,6 +34,17 @@ const config = {
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
 
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/ban-ts-comment': [
+            'warn',
+            {
+                'ts-expect-error': 'allow-with-description',
+                'ts-ignore': 'allow-with-description',
+                'ts-nocheck': 'allow-with-description',
+                'ts-check': false,
+                minimumDescriptionLength: 3,
+            },
+        ],
         '@typescript-eslint/ban-types': [
             'error',
             {
